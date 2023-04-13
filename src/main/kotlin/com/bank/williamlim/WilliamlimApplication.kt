aaab0b2d18c155985b5478e027e5a26f47a363cb
William Lim <william16.lim@gmail.com>
2023-04-13 09:11:51 +0700

    Initial commit

---
+++ src/main/kotlin/com/bank/williamlim/WilliamlimApplication.kt
@@ -0,0 +1,11 @@
+package com.bank.williamlim
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WilliamlimApplication
+
+fun main(args: Array<String>) {
+	runApplication<WilliamlimApplication>(*args)
+}
---
+++ src/test/kotlin/com/bank/williamlim/WilliamlimApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bank.williamlim
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WilliamlimApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
